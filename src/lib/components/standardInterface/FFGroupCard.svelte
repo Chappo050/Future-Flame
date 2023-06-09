6c03cc80f2c841a247e8a77b38b9e0e64f8176fa
Chappo050 <matthewchaplin60@gmail.com>
2023-06-09 23:28:57 +1000

    Changed name

---
+++ src/lib/components/standardInterface/FFGroupCard.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	import { fetchImage, fetchUserProfile } from '$lib/helpers/supabaseHelpers';
+	import { Avatar } from '@skeletonlabs/skeleton';
+
+	import { fade, slide } from 'svelte/transition';
+	import type { SupabaseClient } from '@supabase/supabase-js';
+	export let group: GroupData;
+	export let supabase: SupabaseClient;
+	let src: string;
+	let avatarUrl: string;
+	let loading: boolean = false;
+	let username: string;
+	let avatarLoading: boolean = false;
+
+	const downloadImage = async (path: string) => {
+		loading = true;
+		src = (await fetchImage(path, 'banners', supabase)) as string;
+		loading = false;
+	};
+
+	const loadUser = async (user_id: string) => {
+		avatarLoading = true;
+		const response = await fetchUserProfile(user_id, supabase);
+		if (response) {
+			avatarUrl = response.avatarUrl || '';
+			username = response.userData.username;
+		}
+
+		avatarLoading = false;
+	};
+	$: if (group.bannerImage) downloadImage(group.bannerImage);
+	$: if (group.user_id) loadUser(group.user_id);
+</script>
+
+<a
+	class="card card-hover overflow-hidden max-w-lg"
+	href="/elements/cards"
+	in:fade={{ duration: 500 }}
+>
+	<header>
+		<img {src} class="bg-black/50 w-full aspect-[21/9]" alt="Post" />
+	</header>
+	<div class="p-4 space-y-4">
+		<h3 class="h3" data-toc-ignore>{group.title}</h3>
+		<article class="">
+			<p>
+				{group.description}
+			</p>
+		</article>
+	</div>
+	<hr class="opacity-50" />
+	<footer class="p-4 flex justify-start items-center space-x-4">
+		<Avatar src={avatarUrl} width="w-8" />
+		<div class="flex-auto flex justify-between items-center">
+			<h6 class="font-bold">{username}</h6>
+			<small>On {new Date().toLocaleDateString()}</small>
+		</div>
+	</footer>
+</a>
