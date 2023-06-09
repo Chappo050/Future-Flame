6b266ac0e87a6b1e4408341f4672e9249059d7c6
Chappo050 <matthewchaplin60@gmail.com>
2023-06-09 23:28:43 +1000

    Global types

---
+++ src/ambient.d.ts
@@ -0,0 +1,15 @@
+interface GroupData {
+	id: number;
+	title: string;
+	description: string;
+	bannerImage: string;
+	user_id: string;
+}
+
+interface ProfileData {
+	id: string;
+	updated_at: Date;
+	username: string;
+	avatar_url: string;
+	website: string;
+}
