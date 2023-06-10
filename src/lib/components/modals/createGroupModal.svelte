38595a29da12b7c21fbb20d1e66fe1484a813953
Chappo050 <matthewchaplin60@gmail.com>
2023-06-10 14:52:01 +1000

    Loading Modal

--- src/lib/components/modals/createGroupModal.svelte
+++ src/lib/components/modals/createGroupModal.svelte
@@ -4,13 +4,16 @@
 	import { modalStore } from '@skeletonlabs/skeleton';
 	//Components
 	import BannerUpload from '../global/bannerUpload.svelte';
+	import { APIRequest } from '$lib/helpers/APIHelpers';
+	import { goto } from '$app/navigation';
+	import FfProgessRadial from '../standardInterface/FFProgessRadial.svelte';
 	// Props
 	export let parent: any;
 	//Variables
-	let banner: any;
 	let bannerURL: string;
 	let supabase: SupabaseClient = $modalStore[0].meta.supabase;
-
+	let loading: boolean = false;
+	let isDisabled: boolean;
 	//Functions
 	// Form Data
 	const formData = {
@@ -19,12 +22,25 @@
 		// email: '',
 		bannerImage: ''
 	};
-	$: formData.bannerImage = bannerURL;
+
 	// We've created a custom submit function to pass the response and close the modal.
-	function onFormSubmit(): void {
-		if ($modalStore[0].response) $modalStore[0].response(formData);
+	async function onFormSubmit() {
+		loading = true;
+		console.log('Modal', $modalStore[0]);
+
+		const response = await APIRequest('/groups', 'POST', formData);
+		loading = false;
+
+		if ($modalStore[0].response && response.id)
+			$modalStore[0].response({ success: true, response });
+		else if ($modalStore[0].response) $modalStore[0].response({ success: false, response });
+
 		modalStore.close();
 	}
+
+	$: formData.bannerImage = bannerURL;
+	$: isDisabled = !formData.title || !formData.description || !formData.bannerImage || !bannerURL;
+
 	// Base Classes
 	const cBase = 'card p-4 w-modal shadow-xl space-y-4';
 	const cHeader = 'text-2xl font-bold';
@@ -33,29 +49,39 @@
 
 {#if $modalStore[0]}
 	<div class="modal-example-form {cBase}">
-		<header class={cHeader}>{$modalStore[0].title ?? '(title missing)'}</header>
-		<article>{$modalStore[0].body ?? '(body missing)'}</article>
-		<!-- Enable for debugging: -->
-		<form class="modal-form {cForm}">
-			<BannerUpload {supabase} bind:url={bannerURL} />
-			<label class="label">
-				<span>Title</span>
-				<input class="input" type="text" bind:value={formData.title} placeholder="Enter name..." />
-			</label>
-			<label class="label">
-				<span>Description</span>
-				<input
-					class="input"
-					type="text"
-					bind:value={formData.description}
-					placeholder="Enter description..."
-				/>
-			</label>
-		</form>
-		<!-- prettier-ignore -->
-		<footer class="modal-footer {parent.regionFooter}">
-        <button class="btn {parent.buttonNeutral}" on:click={parent.onClose}>{parent.buttonTextCancel}</button>
-        <button class="btn {parent.buttonPositive}" on:click={onFormSubmit}>Create</button>
-    </footer>
+		{#if loading}
+			<header class={cHeader}>Creating Group Please Wait..</header>
+			<form class="modal-form {cForm} "><FfProgessRadial /></form>
+		{:else}
+			<header class={cHeader}>{$modalStore[0].title ?? '(title missing)'}</header>
+			<article>{$modalStore[0].body ?? '(body missing)'}</article>
+			<!-- Enable for debugging: -->
+			<form class="modal-form {cForm} ">
+				<BannerUpload {supabase} bind:url={bannerURL} />
+				<label class="label">
+					<span>Title</span>
+					<input
+						class="input"
+						type="text"
+						bind:value={formData.title}
+						placeholder="Enter name..."
+					/>
+				</label>
+				<label class="label">
+					<span>Description</span>
+					<input
+						class="input"
+						type="text"
+						bind:value={formData.description}
+						placeholder="Enter description..."
+					/>
+				</label>
+			</form>
+			<!-- prettier-ignore -->
+			<footer class="modal-footer {parent.regionFooter}">
+			<button class="btn {parent.buttonNeutral}" on:click={parent.onClose}>{parent.buttonTextCancel}</button>
+			<button class="btn {parent.buttonPositive}" disabled={isDisabled} on:click={onFormSubmit}>Create</button>
+		</footer>
+		{/if}
 	</div>
 {/if}
